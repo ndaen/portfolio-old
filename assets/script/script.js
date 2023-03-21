5b135149abd888b311259d93328650c55ca62935
Noah DAEN <noah.daen@webedia-group.com>
2023-03-21 10:27:10 +0100

    Correction de bug

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,7 +6,7 @@ const last = document.querySelector('.container.last')
 const right = document.querySelectorAll('.container.right')
 const timeline_title = document.querySelectorAll('.timeline-title')
 
-if(window.location.pathname === '/' | window.location.pathname === '/index.html' ){
+if(window.location.pathname === '/' | window.location.pathname === '/index.html' | window.location.pathname === '/portoflio/' | window.location.pathname === '/portoflio/index.html'){
 
   sidebarToogleBtn.onclick = (e) => {
     e.preventDefault()
@@ -54,7 +54,7 @@ if(document.referrer !==  window.location.href){
   }
 }
 
-if(window.location.pathname === '/' | window.location.pathname === '/index.html'){
+if(window.location.pathname === '/' | window.location.pathname === '/index.html' | window.location.pathname === '/portoflio/' | window.location.pathname === '/portoflio/index.html'){
   // add event to all nav link
   for (let i = 0; i < navigationLinks.length; i++) {
 
@@ -74,7 +74,7 @@ if(window.location.pathname === '/' | window.location.pathname === '/index.html'
   }
 }
 
-if(window.location.pathname === '/' | window.location.pathname === '/index.html'){
+if(window.location.pathname === '/' | window.location.pathname === '/index.html' | window.location.pathname === '/portoflio/' | window.location.pathname === '/portoflio/index.html'){
   const filter = document.querySelectorAll("[data-filter-link]")
   let project = document.querySelectorAll("#project-item")
   let project2 = project
